30c1c48149bdf19554a2885ce13adaca45f9b050
Arthur Nogueira Neves <arthurnn@gmail.com>
2024-10-16 16:21:38 -0400

    improve code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -60,7 +60,7 @@ class ArticlesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_article
-      @article = Article.find(params[:id])
+      @article = Article.better_find_by_id(params)
     end
 
     # Only allow a list of trusted parameters through.
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,5 @@
 class Article < ApplicationRecord
+  def self.better_find_by_id(params)
+    Article.find_by "id = '#{params[:id]}'"
+  end
 end
